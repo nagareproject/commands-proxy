eec091aa3eaf2d9921bf4df790ad2cca62637592
Alain Poirier <alain.poirier@net-ng.com>
2020-05-31 23:07:22 +0200

    Initial checkin

---
+++ nagare/admin/nginx_proxy.py
@@ -0,0 +1,79 @@
+# --
+# Copyright (c) 2008-2020 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from nagare.admin import command
+
+
+class Proxy(command.Command):
+    DESC = 'Nginx reverse proxy dispatch rules generation'
+    WITH_STARTED_SERVICES = True
+    DEFAULT_PROXY_DIRECTIVES = {
+        'proxy_set_header Host $host',
+        'proxy_set_header X-Forwarded-Proto $scheme',
+        'proxy_set_header X-Forwarded-Port $server_port',
+        'proxy_redirect off'
+    }
+    DEFAULT_WEBSOCKET_DIRECTIVES = DEFAULT_PROXY_DIRECTIVES | {
+        'proxy_http_version 1.1',
+        'proxy_set_header Upgrade $http_upgrade',
+        'proxy_set_header Connection "Upgrade"'
+    }
+
+    @staticmethod
+    def generate_directives(directives):
+        return (directive + ';' for directive in sorted(directives))
+
+    def generate_location_directives(self, proxy_service, location, default_location_directives):
+        location_directives = proxy_service.get_location_directives(location, default_location_directives)
+
+        yield 'location {}/ {{'.format(location)
+        for directive in self.generate_directives(location_directives):
+            yield '    ' + directive
+        yield '}\n'
+
+    def generate_dir_directives(self, proxy_service, location, dirname, gzip):
+        default_dir_directives = {'alias {}/'.format(dirname)}
+        if gzip:
+            default_dir_directives.add('gzip_static on')
+
+        for directive in self.generate_location_directives(proxy_service, location, default_dir_directives):
+            yield directive
+
+    def generate_proxy_pass_directives(self, proxy_service, location, default_directives, url=None):
+        is_socket, ssl, endpoint, app_url = proxy_service.endpoint
+        proxy_directive = 'proxy_pass http{}://{}{}{}/'.format(
+            's' if ssl else '',
+            endpoint, ':' if is_socket else '',
+            url or app_url
+        )
+
+        for directive in self.generate_location_directives(
+            proxy_service,
+            location,
+            default_directives | {proxy_directive}
+        ):
+            yield directive
+
+    def generate_app_directives(self, proxy_service, location):
+        for directive in self.generate_proxy_pass_directives(proxy_service, location, self.DEFAULT_PROXY_DIRECTIVES):
+            yield directive
+
+    def generate_ws_directives(self, proxy_service, location):
+        for directive in self.generate_proxy_pass_directives(
+            proxy_service,
+            location,
+            self.DEFAULT_WEBSOCKET_DIRECTIVES,
+            location
+        ):
+            yield directive
+
+    def run(self, http_proxy_service, services_service):
+        """
+        """
+        services_service(http_proxy_service.generate_directives, self)
---
+++ nagare/admin/proxy.py
@@ -0,0 +1,69 @@
+# --
+# Copyright (c) 2008-2020 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from nagare.admin import command
+from nagare.services import plugin
+
+
+class Commands(command.Commands):
+    DESC = 'HTTP reverse proxy dispatch rules generation'
+
+
+class HTTPProxyService(plugin.Plugin):
+    CONFIG_SPEC = {
+        'server': {
+            '__many__': 'boolean'
+        },
+        '__many__': {
+            '__many__': 'boolean'
+        }
+    }
+
+    def __init__(self, name, dist, server, services_service, **locations):
+        services_service(super(HTTPProxyService, self).__init__, name, dist, server=server, **locations)
+
+        self.server = {v for v, activated in server.items() if activated}
+        self.locations = locations
+        self.endpoint = (False, '', '')
+
+    def handle_start(self, app, publisher_service, application_service):
+        publisher = publisher_service.service
+        app = application_service.service
+
+        tcp, ssl, endpoint, _ = publisher.endpoint
+        self.endpoint = (not tcp, ssl, endpoint, app.url)
+
+    @staticmethod
+    def merge_directives(directives, default_directives):
+        directives_on = {v for v, activated in directives.items() if activated}
+        directives_off = {v for v, activated in directives.items() if not activated}
+
+        return (default_directives - directives_off) | directives_on
+
+    def get_server_directives(self, default_directives):
+        return self.merge_directives(self.server, default_directives)
+
+    def get_location_directives(self, location, default_direcives):
+        return self.merge_directives(self.locations.pop(location, {}), default_direcives)
+
+    def generate_directives(self, proxy, statics_service, services_service, reloader_service=None):
+        if reloader_service is not None:
+            services_service(reloader_service.start, None)
+
+        print('\n'.join(services_service(proxy.generate_directives, self.server)))
+        if self.server:
+            print()
+
+        print('\n'.join(statics_service.generate_proxy_directives(self, proxy)))
+        if self.locations:
+            print()
+
+        for location, directives in list(self.locations.items()):
+            directives = {v for v, activated in directives.items() if activated}
+            print('\n'.join(services_service(proxy.generate_location_directives, location, directives)))
